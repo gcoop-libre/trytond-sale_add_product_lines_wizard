b1aefe4169d46e3d270b635b581f1fa2a66d771b
Luciano Rossi <lukio@gcoop.coop>
2019-03-22 05:11:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,101 +1,100 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# This file is part of the sale_add_product_lines_wizard module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from setuptools import setup
 import re
 import os
-import ConfigParser
-
-MODULE = 'sale_add_product_lines_wizard'
-PREFIX = 'gcoop-libre'
-MODULE2PREFIX = {}
+import io
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return io.open(
+        os.path.join(os.path.dirname(__file__), fname),
+        'r', encoding='utf-8').read()
 
 
 def get_require_version(name):
-    if minor_version % 2:
-        require = '%s >= %s.%s.dev0, < %s.%s'
-    else:
-        require = '%s >= %s.%s, < %s.%s'
+    require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
 
-config = ConfigParser.ConfigParser()
+
+config = ConfigParser()
 config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
-
 version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
+name = 'trytonar_sale_add_product_lines_wizard'
+
+download_url = 'https://github.com/gcoop-libre/trytond-sale_add_product_lines_wizard/tree/%s.%s' % (
+    major_version, minor_version)
 
 requires = []
 for dep in info.get('depends', []):
-    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        prefix = MODULE2PREFIX.get(dep, 'trytond')
-        requires.append('%s_%s >= %s.%s, < %s.%s' %
-                (prefix, dep, major_version, minor_version,
-                major_version, minor_version + 1))
+    if not re.match(r'(ir|res)(\W|$)', dep):
+        requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
+dependency_links = []
 
-setup(name='%s_%s' % (PREFIX, MODULE),
+setup(name=name,
     version=version,
-    description='',
+    description='Tryton module wizard to add several lines to Sale',
     long_description=read('README'),
-    author='gcoop Cooperativa de Software Libre',
-    author_email='info@gcoop.coop',
-    url='https://gcoop.coop/',
-    download_url="https://github.coop/gcoop-libre/trytond-%s" % MODULE,
-    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    author='gcoop-libre',
+    url='https://github.com/gcoop-libre/trytond-sale_add_product_lines_wizard',
+    download_url=download_url,
+    package_dir={'trytond.modules.sale_add_product_lines_wizard': '.'},
     packages=[
-        'trytond.modules.%s' % MODULE,
-        'trytond.modules.%s.tests' % MODULE,
+        'trytond.modules.sale_add_product_lines_wizard',
+        'trytond.modules.sale_add_product_lines_wizard.tests',
         ],
     package_data={
-        'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', 'view/*.xml']),
+        'trytond.modules.sale_add_product_lines_wizard': (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: Bulgarian',
-        'Natural Language :: Catalan',
-        'Natural Language :: Czech',
-        'Natural Language :: Dutch',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
-        'Natural Language :: French',
-        'Natural Language :: German',
-        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
     install_requires=requires,
+    dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    %s = trytond.modules.%s
-    """ % (MODULE, MODULE),
+    sale_add_product_lines_wizard = trytond.modules.sale_add_product_lines_wizard
+    """,
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
